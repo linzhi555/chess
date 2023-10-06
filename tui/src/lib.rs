b1a644c111db2db0f548b68f0f4de8302537ee47
lzq <lzq47@foxmail.com>
2023-10-06 23:44:31 +0800

    multithread

--- tui/src/lib.rs
+++ tui/src/lib.rs
@@ -1,87 +1,140 @@
-use std::io::{stdin, stdout, Stdin, Stdout, Write};
+use std::io::{stdin, stdout, Stdout, Write};
+use std::sync::mpsc::Sender;
 use termion::input::TermRead;
 use termion::raw::IntoRawMode;
 use termion::{event::Key, raw::RawTerminal};
 
 pub struct Ui {
-    bufferWidth: isize,
+    cur_pos: usize,
     buffer: Vec<char>,
     stdout: Option<RawTerminal<Stdout>>,
+    message: String,
 }
 
 impl Ui {
     pub fn new() -> Self {
         Ui {
-            bufferWidth: 0,
+            cur_pos: 0,
             buffer: Vec::new(),
             stdout: None,
+            message: String::new(),
         }
+    }
+
+    fn make_string(&self) -> String {
+        let mut s = String::new();
+        for c in self.buffer.iter() {
+            s.push(*c)
+        }
+        s
     }
 
     fn move_cursor(&mut self, i: u16) {
         write!(
             self.stdout.as_mut().unwrap(),
             "{}",
-            termion::cursor::Goto(i, 2)
+            termion::cursor::Goto(i, 1)
         )
         .unwrap();
     }
 
-    pub fn run(&mut self) {
-        let stdin = stdin();
-        let mut stdout = stdout().into_raw_mode().unwrap();
+    fn insert(&mut self, c: char) {
+        self.buffer.insert(self.cur_pos, c);
+        self.cur_pos += 1;
+    }
+    fn delete(&mut self) {
+        if self.cur_pos >= 1 {
+            self.cur_pos -= 1;
+            self.buffer.remove(self.cur_pos);
+        }
+    }
 
+    fn message(&mut self, s: &str) {
+        self.message = s.to_string()
+    }
+    fn clear_all(&mut self){
+        write!(self.stdout.as_mut().unwrap(),"{}", termion::clear::All,).unwrap();
+        self.stdout.as_mut().unwrap().flush().unwrap();
+    }
+
+    fn render(&mut self) {
         write!(
-            stdout,
-            "{}{}q to exit. Type stuff, use alt, and so on.",
+            self.stdout.as_mut().unwrap(),
+            "{}{}{}",
             termion::clear::All,
-            termion::cursor::Goto(1, 1),
+            termion::cursor::Goto(1, 3),
+            self.message,
         )
         .unwrap();
+
+        let mut i = 0;
+        let mut cursor = 0;
+        self.move_cursor(1);
+        write!(
+            self.stdout.as_mut().unwrap(),
+            "{}> ",
+            termion::clear::CurrentLine
+        )
+        .unwrap();
+
+        for c in self.buffer.iter() {
+            print!("{}", c);
+            if i < self.cur_pos {
+                cursor += 1;
+                if !c.is_ascii() {
+                    cursor += 1
+                }
+            }
+            i += 1;
+        }
+        self.move_cursor(cursor + 3);
+        self.stdout.as_mut().unwrap().flush().unwrap();
+    }
+
+    pub fn run(&mut self,tx:Sender<String>) {
+        let stdin = stdin();
+        let mut stdout = stdout().into_raw_mode().unwrap();
         stdout.flush().unwrap();
         self.stdout = Some(stdout);
-        let mut i = 1;
+
+        self.message("q to exit. Type stuff, use alt, and so on.");
+        self.render();
         for c in stdin.keys() {
-            self.stdout.as_mut().unwrap().flush().unwrap();
             match c.unwrap() {
-                Key::Char(c) => {
-                    print!("{}", c);
-                    if !c.is_ascii() {
-                        i += 2;
-                        self.move_cursor(i);
-                    } else {
-                        i += 1;
-                        self.move_cursor(i);
-                    }
+                Key::Char('\n') => {
+                    self.cur_pos = 0;
+                    let s = self.make_string();
+                    self.message(&s);
+                    self.buffer.clear();
+                    tx.send(s.clone()).unwrap();
                 }
-                Key::Alt(c) => {
-                    print!("^{}", c);
-                    i += 2;
-                    self.move_cursor(i);
+                Key::Char(c) => {
+                    if c.is_alphanumeric() {
+                        self.insert(c)
+                    }
                 }
                 Key::Ctrl('d') => {
                     break;
                 }
-                //Key::Esc => print!("ESC"),
                 Key::Left => {
-                    i -= 1;
-                    self.move_cursor(i);
+                    if self.cur_pos >= 1 {
+                        self.cur_pos -= 1
+                    }
                 }
                 Key::Right => {
-                    i += 1;
-                    self.move_cursor(i);
+                    if self.cur_pos <= self.buffer.len() {
+                        self.cur_pos += 1
+                    }
                 }
+                Key::End => self.cur_pos = self.buffer.len(),
+                Key::Home => self.cur_pos = 0,
                 //Key::Up => print!("↑"),
                 //Key::Down => print!("↓"),
-                Key::Backspace => {
-                    print!(" ");
-                    i += 1;
-                    self.move_cursor(i);
-                }
+                Key::Backspace => self.delete(),
                 _ => {}
             }
-
-            self.stdout.as_mut().unwrap().flush().unwrap();
+            self.render();
         }
+        self.clear_all();
     }
 }
--- tui/src/main.rs
+++ tui/src/main.rs
@@ -1,8 +1,22 @@
+use std::sync::mpsc;
+use std::thread;
+use std::time::Duration;
 use tui::Ui;
-
 fn main() {
+
+    let(tx,rx)=mpsc::channel();
         let mut ui = Ui::new();
-        ui.run();
+    let handle = thread::spawn(move || {
+        ui.run(tx);
+    });
+    
+    let mut buffer = String::new();
+    while !handle.is_finished() {
+        if let Ok(temp)=rx.recv_timeout(Duration::from_millis(100)){
+            buffer.push_str(temp.as_str());
+        }
+    }
+    println!("the result is {}",buffer)
 
 }
 
